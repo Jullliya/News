75a56598f2cd5fde089a8fd036dfda39d65c2079
Jullliya <vamoyu2@gmail.com>
2024-09-14 22:33:42 +0300

    [feature] fragments ui implementation

---
+++ app/src/main/java/dev/jullls/news/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.jullls.news
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_home)
+    }
+}
---
+++ app/src/main/java/dev/jullls/news/NavigationFragment.kt
@@ -0,0 +1,4 @@
+package dev.jullls.news
+
+class NavigationFragment {
+}
